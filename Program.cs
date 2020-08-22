2ef631d50efd34a216c65f5f68baedc9a822eec4
SamuelDeniz9139 <69916494+SamuelDeniz9139@users.noreply.github.com>
2020-08-22 12:35:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//name: Sam Deniz
+namespace Deniz_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//prints the window and the message
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
